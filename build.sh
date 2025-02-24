b83a9042183dbe68eec8b804e8dd0cd01d77f6fe
mham <warofzen1@proton.me>
2025-02-24 07:55:12 +0700

    sunshine

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux 
+# dnf install -y tmux
 
 # Use a COPR Example:
 #
@@ -21,4 +21,8 @@ dnf install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+
+dnf5 -y copr enable lizardbyte/stable
+dnf5 -y install Sunshine
+dnf5 -y copr disable lizardbyte/stable
